6c8c828ecceef2d053134ff0bb59f30a6aa3f85e
fatimabk1 <fatima.kahbi@gmail.com>
2024-03-29 17:21:16 -0400

    Initial Commit

---
+++ BasicCalculator/BasicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorApp.swift
+//  BasicCalculator
+//
+//  Created by Fatima Kahbi on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
